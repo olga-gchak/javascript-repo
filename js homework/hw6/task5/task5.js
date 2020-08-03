29d697210929bebfbee4e46ac6eb9591dba62731
olga-gchak <67831961+olga-gchak@users.noreply.github.com>
2020-08-03 10:01:38 +0300

    Create task5.js

---
+++ js homework/hw6/task5/task5.js
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+</head>
+<body>
+    <h1>I am a big header!!!</h1>
+    <div id="myDiv">
+        <p>First paragraph</p>
+        <p>Second paragraph</p>
+        <p>Third paragraph</p>
+        <p>Fourth paragraph</p>
+    </div>
+    <ul id="myList">
+        <li>Make</li>
+        <li>me</li>
+        <li>horizontal!</li>
+    </ul>
+    <span>Make me invisible, please!</span>
+
+    <script>
+        let elem1 = document.getElementsByTagName("h1")[0];
+        elem1.style.backgroundColor = "chartreuse";
+        let elem2 = document.getElementById("myDiv");
+        elem2.firstElementChild.style.fontWeight = "bold";
+        elem2.firstElementChild.nextElementSibling.style.color = "red";
+        elem2.lastElementChild.previousElementSibling.style.textDecoration = "underline";
+        elem2.lastElementChild.style.fontStyle = "italic";
+        let elem3 = document.getElementById("myList");
+        elem3.firstElementChild.style.display = "inline";
+        elem3.lastElementChild.style.display = "inline";
+        elem3.firstElementChild.nextElementSibling.style.display = "inline";
+        let elem4 = document.getElementsByTagName("span")[0];
+        elem4.style.visibility = "hidden";
+    </script>
+</body>
+</html>
