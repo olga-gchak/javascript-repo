5f5d449951c0f5018226583e0a9f7c1179ca935b
olga-gchak <67831961+olga-gchak@users.noreply.github.com>
2020-08-20 12:07:54 +0300

    Update server.js

--- js homework/hw11/task1/server.js
+++ js homework/hw11/task1/server.js
@@ -4,8 +4,9 @@ http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/html'});
   const os = require('os');
   const path = require('path');
-  
+  const seconds = os.uptime();
+  const minutes = seconds/60;
   response.end("<h1> System information</h1> Current user name: " + os.userInfo().username + "<br/>" + "OS type: " + os.type() +
-  "<br/>" + "System work time: " + os.uptime() + "<br/>" + "Current work directory: " + path.dirname(__filename) + "<br/>" + "Server file name: " + 
+  "<br/>" + "System work time: " + minutes.toFixed(2) +" minutes" + "<br/>" + "Current work directory: " + path.dirname(__filename) + "<br/>" + "Server file name: " + 
   path.basename(__filename));
 }).listen(5000);
